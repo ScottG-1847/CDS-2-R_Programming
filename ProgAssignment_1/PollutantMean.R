ce552d1087ec2123c723a235a2b5e19dc20c3b67
ScottG-1847 <ScottG.GitHub@gmail.com>
2015-03-13 11:15:36 -0500

    Rewrite Complete()

--- ProgAssignment_1/PollutantMean.R
+++ ProgAssignment_1/PollutantMean.R
@@ -1,37 +1,37 @@
-complete <- function(directory, pollutant, id = 1:332) {
-    iVec <- vector(mode="numeric", length=0)
-    nVec <- vector(mode="numeric", length=0)
-    
-    for (i in id){
-        ## Get data file name
-        iStr <- toString(i)
-        iLen <- nchar(iStr)
-        switch(iLen,
-               pad <- "00",
-               pad <- "0",
-               pad <- ""    
-        )
-        
-        fileName <- paste(directory, "/", pad, i, ".csv", sep="")
-        
-        ## Open data file
-        df <- read.csv(fileName)
-        
-        ## Count complete cases
-        n <- sum(complete.cases(df))
-        
-        ## Append to holding vectors
-        iVec <- c(iVec, i)
-        nVec <- c(nVec, n)
-
-        ## Unload data frame
-        rm(df)
-    }    
-    
-    result <- data.frame(iVec, nVec)
-    
+complete <- function(directory, fid = 1:332) {
+	id <- vector(mode="numeric", length=0)
+	nobs <- vector(mode="numeric", length=0)
+	
+	for (i in fid){
+		## Get data file name
+		iStr <- toString(i)
+		iLen <- nchar(iStr)
+		
+		switch(iLen,
+		pad <- "00",
+		pad <- "0",
+		pad <- ""
+		)
+		
+		fileName <- paste(directory, "/", pad, i, ".csv", sep="")
+		
+		## Open data file
+		df <- read.csv(fileName)
+		
+		## Count complete cases
+		n <- sum(complete.cases(df))
+		
+		id <- c(id, i)
+		nobs <- c(nobs, n)
+		
+		## Unload source data
+		rm(df)
+		
+	}
+	
+	result <- data.frame(id, nobs)
+	print(result)
 }
-
 
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
