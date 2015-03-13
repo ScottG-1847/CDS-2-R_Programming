0a0be23002b84b4ad38e3d9ba7be52af2d24f573
ScottG-1847 <ScottG.GitHub@gmail.com>
2015-03-13 12:21:07 -0500

    Create corr.R

---
+++ ProgAssignment_1/corr---function-directory-threshold-0-nitrates---vector-mode-numeric-length-0-sulfates---vector-mode-numeric-length-0-correlations---vector-mode-numeric-length-0-for-i-in-1-332-Get-data-file-name-iStr---toString-i-iLen---nchar-iStr-switch-iLen-pad---00-pad---0-pad---fileName---paste-directory/corr.R
@@ -0,0 +1,45 @@
+corr <- function(directory, threshold = 0) {
+	nitrates <- vector(mode="numeric", length=0)
+	sulfates <- vector(mode="numeric", length=0)
+	correlations <- vector(mode="numeric", length=0)
+	
+	for (i in 1:332){
+		## Get data file name
+		iStr <- toString(i)
+		iLen <- nchar(iStr)
+		
+		switch(iLen,
+		pad <- "00",
+		pad <- "0",
+		pad <- ""
+		)
+		
+		fileName <- paste(directory, "/", pad, i, ".csv", sep="")
+		
+		## Open data file
+		df <- read.csv(fileName)
+		
+		## Count complete cases
+		cc <- sum(complete.cases(df))
+
+		if (cc >= threshold) {
+		## save the data
+			df2 <- df[complete.cases(df),]
+			
+			n <- df2$nitrate
+			s <- df2$sulfate
+			
+			c <- cor(s, n)
+			c <- round(c, 5)
+			correlations <- c(correlations, c)
+		}
+		
+		## Unload source data
+		rm(df)
+		
+	}
+	
+	correlations
+	
+
+}
