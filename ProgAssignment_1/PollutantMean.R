bb90ccf58ac98fad2ed70aeb943d8473b643d09c
ScottG-1847 <s_goetsch@yahoo.com>
2015-03-12 20:19:18 -0500

    Update

--- ProgAssignment_1/PollutantMean.R
+++ ProgAssignment_1/PollutantMean.R
@@ -1,35 +1,66 @@
-rl <- function(){
-    source("PollutantMean.R")
+complete <- function(directory, pollutant, id = 1:332) {
+    iVec <- vector(mode="numeric", length=0)
+    nVec <- vector(mode="numeric", length=0)
+    
+    for (i in id){
+        ## Get data file name
+        iStr <- toString(i)
+        iLen <- nchar(iStr)
+        switch(iLen,
+               pad <- "00",
+               pad <- "0",
+               pad <- ""    
+        )
+        
+        fileName <- paste(directory, "/", pad, i, ".csv", sep="")
+        
+        ## Open data file
+        df <- read.csv(fileName)
+        
+        ## Count complete cases
+        n <- sum(complete.cases(df))
+        
+        ## Append to holding vectors
+        iVec <- c(iVec, i)
+        nVec <- c(nVec, n)
+
+        ## Unload data frame
+        rm(df)
+    }    
+    
+    result <- data.frame(iVec, nVec)
+    
 }
+
+
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
+    ## Create vector to hold values
+    vals <- vector(mode = "numeric", length=0)
+    
     for (i in id){
-        ## Load a data file
-        fn <- fileName(directory, i)
-        df <- read.csv(fn)
+        ## Get data file name
+        iStr <- toString(i)
+        iLen <- nchar(iStr)
+        switch(iLen,
+               pad <- "00",
+               pad <- "0",
+               pad <- ""    
+        )
         
-        ## Do something with the file
-        str <- paste(i, ".csv - ", nrow(df))
-        print(str)
+        fileName <- paste(directory, "/", pad, i, ".csv", sep="")
         
-        ## Unload DataFrame
+        ## Open data file
+        df <- read.csv(fileName)
+        
+        ## Save pollutant values
+        vals <- c(vals, df[ , pollutant])
+
+        ## Unload data frame
         rm(df)
     }
-}
-
-fileName <- function(directory, i){
-## Returns a string that is the directory/file to open
-## Needed because i may need leading 0's added
     
-    iStr <- toString(i)
-    iLen <- nchar(iStr)
-    switch(iLen,
-        slashPad <- "/00",
-        slashPad <- "/0",
-        slashPad <- "/"    
-    )
-    
-    result <- paste(directory, slashPad, str, ".csv", sep="")
-    result
-
+    ## Calculate & Return data
+    m <-mean(vals, na.rm=TRUE)
+    round(m, digits = 3)
 }
