ee462dbc620e89bd8e13dbfdee08d0a6b8f29615
ScottG-1847 <ScottG.GitHub@gmail.com>
2015-03-13 11:22:09 -0500

    Create complete.r

---
+++ ProgAssignment_1/complete.r
@@ -0,0 +1,34 @@
+complete <- function(directory, fid = 1:332) {
+	id <- vector(mode="numeric", length=0)
+	nobs <- vector(mode="numeric", length=0)
+	
+	for (i in fid){
+		## Get data file name
+		iStr <- toString(i)
+		iLen <- nchar(iStr)
+		
+		switch(iLen,
+		pad <- "00",
+		pad <- "0",
+		pad <- ""
+		)
+		
+		fileName <- paste(directory, "/", pad, i, ".csv", sep="")
+		
+		## Open data file
+		df <- read.csv(fileName)
+		
+		## Count complete cases
+		n <- sum(complete.cases(df))
+		
+		id <- c(id, i)
+		nobs <- c(nobs, n)
+		
+		## Unload source data
+		rm(df)
+		
+	}
+	
+	result <- data.frame(id, nobs)
+	result
+}
